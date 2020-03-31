7afcc6419a26b3ebad146b96dc3d1b620b4a254c
Marcello Albuquerque <marcello.albuca@gmail.com>
2020-03-30 21:24:27 -0300

    Implementando WebViewClient para abrir link na mesma WebView

--- app/src/main/java/com/example/examplewebview/MainActivity.java
+++ app/src/main/java/com/example/examplewebview/MainActivity.java
@@ -4,17 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String website = "http://aftersound.belweb.com.br/";
-        WebView myWebView = (WebView) findViewById(R.id.webview);
-        myWebView.loadUrl(website);
+        mWebView = (WebView) findViewById(R.id.webview);
+        mWebView.loadUrl("http://aftersound.belweb.com.br/");
 
+        mWebView.setWebViewClient(new WebViewClient(){
+            public boolean shouldOverrideUrlLoading(WebView view, String url){
+                view.loadUrl(url);
+                return false;
+            }
+        });
     }
 }
